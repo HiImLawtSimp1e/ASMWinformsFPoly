35ffb41540b8c09d69335f96e608b5b1eb71d28a
My PC <My PC@LAPTOP-TJU16KH0>
2023-08-15 01:30:48 +0700

    Add project files.

---
+++ GameCaro/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace GameCaro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl_ChessBoard = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pctb_Avatar = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.rdoBtn_15x15 = new System.Windows.Forms.RadioButton();
+            this.rdoBtn_10x10 = new System.Windows.Forms.RadioButton();
+            this.rdoBtn_5x5 = new System.Windows.Forms.RadioButton();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctb_Avatar)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_ChessBoard
+            // 
+            this.pnl_ChessBoard.BackColor = System.Drawing.SystemColors.Control;
+            this.pnl_ChessBoard.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnl_ChessBoard.Location = new System.Drawing.Point(14, 22);
+            this.pnl_ChessBoard.Name = "pnl_ChessBoard";
+            this.pnl_ChessBoard.Size = new System.Drawing.Size(450, 450);
+            this.pnl_ChessBoard.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.pctb_Avatar);
+            this.panel2.Location = new System.Drawing.Point(478, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(269, 269);
+            this.panel2.TabIndex = 1;
+            // 
+            // pctb_Avatar
+            // 
+            this.pctb_Avatar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pctb_Avatar.BackgroundImage = global::GameCaro.Properties.Resources.caro;
+            this.pctb_Avatar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctb_Avatar.InitialImage = global::GameCaro.Properties.Resources.caro;
+            this.pctb_Avatar.Location = new System.Drawing.Point(5, 3);
+            this.pctb_Avatar.Name = "pctb_Avatar";
+            this.pctb_Avatar.Size = new System.Drawing.Size(264, 264);
+            this.pctb_Avatar.TabIndex = 0;
+            this.pctb_Avatar.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.rdoBtn_15x15);
+            this.panel3.Controls.Add(this.rdoBtn_10x10);
+            this.panel3.Controls.Add(this.rdoBtn_5x5);
+            this.panel3.Controls.Add(this.btn_Start);
+            this.panel3.Location = new System.Drawing.Point(478, 307);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(268, 133);
+            this.panel3.TabIndex = 2;
+            // 
+            // rdoBtn_15x15
+            // 
+            this.rdoBtn_15x15.AutoSize = true;
+            this.rdoBtn_15x15.Location = new System.Drawing.Point(33, 103);
+            this.rdoBtn_15x15.Name = "rdoBtn_15x15";
+            this.rdoBtn_15x15.Size = new System.Drawing.Size(54, 17);
+            this.rdoBtn_15x15.TabIndex = 3;
+            this.rdoBtn_15x15.TabStop = true;
+            this.rdoBtn_15x15.Text = "15x15";
+            this.rdoBtn_15x15.UseVisualStyleBackColor = true;
+            // 
+            // rdoBtn_10x10
+            // 
+            this.rdoBtn_10x10.AutoSize = true;
+            this.rdoBtn_10x10.Location = new System.Drawing.Point(33, 80);
+            this.rdoBtn_10x10.Name = "rdoBtn_10x10";
+            this.rdoBtn_10x10.Size = new System.Drawing.Size(54, 17);
+            this.rdoBtn_10x10.TabIndex = 2;
+            this.rdoBtn_10x10.TabStop = true;
+            this.rdoBtn_10x10.Text = "10x10";
+            this.rdoBtn_10x10.UseVisualStyleBackColor = true;
+            // 
+            // rdoBtn_5x5
+            // 
+            this.rdoBtn_5x5.AutoSize = true;
+            this.rdoBtn_5x5.Location = new System.Drawing.Point(33, 57);
+            this.rdoBtn_5x5.Name = "rdoBtn_5x5";
+            this.rdoBtn_5x5.Size = new System.Drawing.Size(42, 17);
+            this.rdoBtn_5x5.TabIndex = 1;
+            this.rdoBtn_5x5.TabStop = true;
+            this.rdoBtn_5x5.Text = "5x5";
+            this.rdoBtn_5x5.UseVisualStyleBackColor = true;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Start.Font = new System.Drawing.Font("Times", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Start.Location = new System.Drawing.Point(5, 3);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(131, 48);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Bắt đầu";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 511);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnl_ChessBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Game Caro";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctb_Avatar)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_ChessBoard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pctb_Avatar;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RadioButton rdoBtn_15x15;
+        private System.Windows.Forms.RadioButton rdoBtn_10x10;
+        private System.Windows.Forms.RadioButton rdoBtn_5x5;
+        private System.Windows.Forms.Button btn_Start;
+    }
+}
+
---
+++ GameCaro/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCaro
+{
+    public partial class Form1 : Form
+    { 
+        int CHESS_WIDTH;
+        int CHESS_HEIGHT;
+
+        int CHESS_BOARD_WIDTH;
+        int CHESS_BOARD_HEIGHT;
+        private List<Player> pler;
+
+        internal List<Player> Pler { get => pler; set => pler = value; }
+
+        private int currentPlayer;
+        public int CurrentPlayer { get => currentPlayer; set => currentPlayer = value; }
+
+        private List<List<Button>> matrix;
+        public List<List<Button>> Matrix { get => matrix; set => matrix = value; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Pler = new List<Player>()
+            {
+                new Player("O",Image.FromFile(Application.StartupPath + "\\Image\\O.PNG")),
+                new Player("X",Image.FromFile(Application.StartupPath + "\\Image\\X.PNG")),
+            };
+            CurrentPlayer = 0;
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            pnl_ChessBoard.Controls.Clear();
+            if(rdoBtn_15x15.Checked)
+            {
+                CHESS_WIDTH = 30;
+                CHESS_HEIGHT = 30;
+
+                CHESS_BOARD_WIDTH = 15;
+                CHESS_BOARD_HEIGHT = 15;
+            }
+            else if (rdoBtn_5x5.Checked)
+            {
+                CHESS_WIDTH = 90;
+                CHESS_HEIGHT = 90;
+
+                CHESS_BOARD_WIDTH = 5;
+                CHESS_BOARD_HEIGHT = 5;
+            }
+            else if (rdoBtn_10x10.Checked)
+            {
+                CHESS_WIDTH = 45;
+                CHESS_HEIGHT = 45;
+
+                CHESS_BOARD_WIDTH = 10;
+                CHESS_BOARD_HEIGHT = 10;
+            }
+            DrawChessBoard();
+        }
+
+        void DrawChessBoard()
+        {
+            Matrix = new List<List<Button>>(); //
+            Button oldBtn = new Button() { Width = 0, Location = new Point(0, 0) };
+            for (int i = 0;i < CHESS_BOARD_HEIGHT; i++)
+            {
+                Matrix.Add(new List<Button>()); //
+                for (int j =0;j <= CHESS_BOARD_WIDTH; j++)
+                {
+                    Button btn = new Button()
+                    {
+                        Width = CHESS_WIDTH,
+                        Height = CHESS_HEIGHT,
+                        Location = new Point(oldBtn.Location.X + oldBtn.Width, oldBtn.Location.Y),
+                        BackgroundImageLayout = ImageLayout.Stretch,
+                        Tag = i.ToString() //
+                    };
+
+                    btn.Click += Btn_Click;
+
+                    pnl_ChessBoard.Controls.Add(btn);
+
+                    Matrix[i].Add(btn); //
+
+                    oldBtn = btn;
+                }
+                oldBtn.Location = new Point(0, oldBtn.Location.Y + CHESS_HEIGHT);
+                oldBtn.Width = 0;
+                oldBtn.Height = 0;
+            }
+            
+        }
+
+        private bool isEndGame(Button btn)
+        {
+            return winHorizontal(btn) || winVertical(btn) || winDepth1(btn) || winDepth2(btn);
+        }
+
+        private Point GetChessPoint(Button btn)
+        {
+            int vertical = Convert.ToInt32(btn.Tag);
+            int horizontal = Matrix[vertical].IndexOf(btn);
+
+            Point point = new Point(horizontal,vertical);
+
+            return point;
+        }
+        private bool winHorizontal(Button btn)
+        {
+            Point point = GetChessPoint(btn);
+
+            int countLeft = 0;
+            for(int i = point.X; i >= 0; i--)
+            {
+                if (Matrix[point.Y][i].BackgroundImage == btn.BackgroundImage)
+                {
+                    countLeft++;
+                }
+                else
+                    break;
+            }
+            int countRight = 0;
+            for (int i = point.X + 1; i < CHESS_BOARD_WIDTH; i++)
+            {
+                if (Matrix[point.Y][i].BackgroundImage == btn.BackgroundImage)
+                {
+                    countRight++;
+                }
+                else
+                    break;
+            }
+
+            return countLeft + countRight >= 5;
+        }
+        private bool winVertical(Button btn)
+        {
+            Point point = GetChessPoint(btn);
+
+            int countTop = 0;
+            for (int i = point.Y; i >= 0; i--)
+            {
+                if (Matrix[i][point.X].BackgroundImage == btn.BackgroundImage)
+                {
+                    countTop++;
+                }
+                else
+                    break;
+            }
+            int countBot = 0;
+            for (int i = point.Y + 1; i < CHESS_BOARD_HEIGHT; i++)
+            {
+                if (Matrix[i][point.X].BackgroundImage == btn.BackgroundImage)
+                {
+                    countBot++;
+                }
+                else
+                    break;
+            }
+
+            return countTop + countBot >= 5;
+        }
+        private bool winDepth1(Button btn)
+        {
+            Point point = GetChessPoint(btn);
+
+            int countTop = 0;
+            for (int i = 0; i <= point.X; i++)
+            {
+                if (point.X - i < 0 || point.Y - i < 0)
+                    break;
+                if (Matrix[point.Y - i][point.X - i].BackgroundImage == btn.BackgroundImage)
+                {
+                    countTop++;
+                }
+                else
+                    break;
+            }
+            int countBot = 0;
+            for (int i = 1; i <= CHESS_BOARD_WIDTH - point.X; i++)
+            {
+                if (point.Y + i >= CHESS_BOARD_HEIGHT || point.X + i >= CHESS_BOARD_WIDTH)
+                    break;
+                if (Matrix[point.Y + i][point.X + i].BackgroundImage == btn.BackgroundImage)
+                {
+                    countBot++;
+                }
+                else
+                    break;
+            }
+
+            return countTop + countBot >= 5;
+        }
+        private bool winDepth2(Button btn)
+        {
+            Point point = GetChessPoint(btn);
+
+            int countTop = 0;
+            for (int i = 0; i <= point.X; i++)
+            {
+                if (point.X + i > CHESS_BOARD_WIDTH || point.Y - i < 0)
+                    break;
+                if (Matrix[point.Y - i][point.X + i].BackgroundImage == btn.BackgroundImage)
+                {
+                    countTop++;
+                }
+                else
+                    break;
+            }
+            int countBot = 0;
+            for (int i = 1; i <= CHESS_BOARD_WIDTH - point.X; i++)
+            {
+                if (point.Y + i >= CHESS_BOARD_HEIGHT || point.X - i < 0)
+                    break;
+                if (Matrix[point.Y + i][point.X - i].BackgroundImage == btn.BackgroundImage)
+                {
+                    countBot++;
+                }
+                else
+                    break;
+            }
+
+            return countTop + countBot >= 5;
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.BackgroundImage != null)
+                return;
+
+            btn.BackgroundImage = Pler[CurrentPlayer].Mark;
+
+            if (isEndGame(btn)) //
+            {
+                MessageBox.Show("Người chơi " + Pler[CurrentPlayer].Name + " đã thắng");
+            }
+
+            CurrentPlayer = CurrentPlayer == 1 ? 0 : 1;
+        }
+
+    }
+}
